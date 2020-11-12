3c4af63fe7bf245d5a6f35087bc36a42e7c92a17
SeGrom <Segrom2001@mail.ru>
2020-11-12 10:49:33 +0300

    Setup

---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityTemplateProjects;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public struct Star
+{
+    public float X;
+    public float Y;
+    public float Z;
+    public float Vx;
+    public float Vy;
+    public float Vz;
+    public float Ax;
+    public float Ay;
+    public float Az;
+    public float Radius;
+    public float Mass;
+}
+
+
+public class StarManager : MonoBehaviour
+{
+    [SerializeField] private List<Material> starClassMats;
+    [SerializeField] private Material blackHoleMat;
+    [SerializeField]private ComputeShader gravityShader;
+    [SerializeField]private StarObject starPref;
+    private ComputeBuffer _starsBuffer;
+    [SerializeField]private Star[] stars;
+    private StarObject[] _starsObjects;
+    [SerializeField]private int[] Counter;
+    private const int GroupsCount = 3; 
+    private const int GroupsSize = 3; 
+    private const int KernelCounts = 32; 
+    private const int KernelSize = 32;
+    private const int centerCount = 200;
+    private int _accelerationKernel,_moveKernel;
+    
+
+    private void Start(){
+        int starCount = GroupsCount * GroupsSize * KernelCounts * KernelSize;
+        stars = new Star[starCount];
+        _starsObjects = new StarObject[starCount];
+        for (int i = 0; i < starCount-centerCount; i++){
+            CreateStar(i,100,0.2f);
+        }
+        for (int i = 0; i < centerCount; i++){
+            CreateStar(starCount-centerCount+i,10, 0.002f);
+        }
+
+        _accelerationKernel=  gravityShader.FindKernel("k_star_acceleration");
+
+        _starsBuffer = new ComputeBuffer(stars.Length,44,ComputeBufferType.Structured);
+        
+        gravityShader.SetBuffer(_accelerationKernel,"starsBuffer",_starsBuffer);
+        gravityShader.SetInt("_kernel_counts",KernelCounts);
+        gravityShader.SetInt("_kernel_size",KernelSize);
+        gravityShader.SetInt("_groups_count",GroupsCount);
+        gravityShader.SetInt("_groups_size",GroupsSize);
+        
+        _starsBuffer.SetData(stars);
+    }
+
+    private void Update(){
+        _starsBuffer.SetData(stars);
+
+        gravityShader.Dispatch(_accelerationKernel,GroupsCount,GroupsSize,1);
+
+        _starsBuffer.GetData(stars);
+        Counter = new int[Enum.GetNames(typeof(SpectralClass)).Length];
+        for (int i = 0; i < stars.Length; i++){
+            var s = stars[i];
+            _starsObjects[i].transform.position = new Vector3(s.X,s.Y,s.Z);
+            _starsObjects[i].options = stars[i];
+            Counter[(int) _starsObjects[i].spectralClass]++;
+        }
+    }
+
+    ~StarManager(){
+        _starsBuffer.Release();
+    }
+
+    StarObject CreateStar(int i, float creationRadius, float velocityRange){
+        var starObj = Instantiate(starPref,Vector3.zero, Quaternion.identity);
+        _starsObjects[i] = starObj;
+        starObj.Initialization();
+        if (starObj.isBlackHole){
+            starObj.name = $"Black Hole ({i})";
+            starObj.GetComponent<MeshRenderer>().material = blackHoleMat;
+            var d =20;
+            starObj.transform.localScale =new Vector3(d,d,d);
+            starObj.GetComponent<LODGroup>().enabled = false;
+            starObj.lodSprite.gameObject.SetActive(false);
+        }
+        else{
+            starObj.name = $"Star {starObj.spectralClass}{starObj.luminosityClass} ({i})";
+            starObj.GetComponent<MeshRenderer>().material = starClassMats[(int) starObj.spectralClass];
+            starObj.GetComponent<MeshRenderer>().material.SetFloat("_luminosity",(float)starObj.luminosity);
+            var d = starObj.options.Radius * 2*5;
+            starObj.transform.localScale =new Vector3(d,d,d);
+            starObj.SetLodColor(starObj.GetComponent<MeshRenderer>().material.GetColor("_spector"));
+
+        }
+        stars[i] = starObj.options;
+        stars[i].X = Random.Range(-creationRadius,creationRadius);
+        stars[i].Y = Random.Range(-creationRadius,creationRadius);
+        stars[i].Z = Random.Range(-creationRadius,creationRadius);
+        stars[i].Vx = Random.Range(-velocityRange,velocityRange);
+        stars[i].Vy =  Random.Range(-velocityRange,velocityRange);
+        stars[i].Vz =  Random.Range(-velocityRange,velocityRange);
+        stars[i].Ax = stars[i].Ay = stars[i].Az = 0;
+        
+        return starObj;
+    }
+}
---
+++ Assets/Scripts/StarObject.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UnityTemplateProjects
+{
+    
+    public enum SpectralClass //Basic (Harvard) Spectral Classification
+    {
+        W=0,O=1,B=2,A=3,F=4,G=5,K=6,M=7,L=8,T=9,Y=10
+    }
+
+    public enum LuminosityClass
+    {
+        O=0,Iap=1,Ia=2,Ib=3,II=4,III=5,IV=6,V=7,VI=8,VII=9
+    }
+
+    public class StarObject : MonoBehaviour
+    {
+        private const double sigma= 5.67e-8d;//Stefan-Boltzmann Constant
+        public Star options;
+        public SpriteRenderer lodSprite;
+        public double temperature;
+        public double  luminosity;
+        public bool isBlackHole = false;
+        public SpectralClass spectralClass{ get; private set; }
+        public LuminosityClass luminosityClass{ get; private set; }
+
+        
+        public void Initialization(){
+            if (Random.Range(0f, 100f) > 99.9999f){
+                isBlackHole = true;
+                luminosity = 0;
+                temperature = 0;
+                options.Radius = 120;
+                options.Mass = 4000000;
+                return;
+            }
+            if (Random.Range(0, 100) > 10){
+                luminosityClass = (LuminosityClass)Random.Range(7,9);
+                int sc = Random.Range(0, 10) < 2 ? Random.Range(2, 9) : Random.Range(4, 7);
+                spectralClass = (SpectralClass)sc;
+            }
+            else{
+                if (Random.Range(0, 100) > 10){
+                    luminosityClass = (LuminosityClass)Random.Range(4,6);
+                    int sc = Random.Range(0, 10) < 2 ? Random.Range(2, 9) : Random.Range(5, 9);
+                    spectralClass = (SpectralClass)sc;
+                }
+                else if (Random.Range(0, 100) > 30){
+                    luminosityClass = (LuminosityClass)Random.Range(2,4);
+                    spectralClass = (SpectralClass) Random.Range(1, 9);
+                }
+                else if (Random.Range(0, 100) > 20){
+                    luminosityClass = (LuminosityClass)Random.Range(7,10);
+                    if(luminosityClass==LuminosityClass.VII) spectralClass = (SpectralClass) Random.Range(1, 5);
+                    else spectralClass = (SpectralClass) Random.Range(7, 11);
+                }
+                else{
+                    luminosityClass = (LuminosityClass)Random.Range(0,2);
+                    spectralClass = (SpectralClass) Random.Range(2, 8);
+                }
+            }
+            luminosity = SetLuminosityByClass();
+            temperature = SetTemperatureByClass();
+            options.Radius =(float) Math.Sqrt(luminosity / (4 * Math.PI * sigma * Math.Pow(temperature, 4)));
+            options.Mass = GetMassFromLum();
+        }
+
+        private float GetMassFromLum(){
+            if (luminosity < 0.0330145) return (float)Math.Pow(luminosity / 0.23d, 0.44444d);
+            else if (luminosity <16) return (float)Math.Pow(luminosity , 0.25d);
+            else if (luminosity < 54000) return (float)Math.Pow(luminosity/1.5, 0.22222d);
+            else return (float)(luminosity/3200);
+        }
+
+        private double SetTemperatureByClass(){
+            switch (spectralClass){
+                case SpectralClass.W: return Random.Range(40000, 70000);
+                case SpectralClass.O: return Random.Range(25000, 40000);
+                case SpectralClass.B: return Random.Range(10000, 25000);
+                case SpectralClass.A: return Random.Range(7000, 10000);
+                case SpectralClass.F: return Random.Range(6000, 7000);
+                case SpectralClass.G: return Random.Range(5000, 6000);
+                case SpectralClass.K: return Random.Range(3500, 5000);
+                case SpectralClass.M: return Random.Range(2500, 3500);
+                case SpectralClass.L: return Random.Range(1500, 2500);
+                case SpectralClass.T: return Random.Range(700, 1500);
+                case SpectralClass.Y: return Random.Range(500, 700);
+                default: throw new Exception($"Unknown star spectral class {spectralClass}");
+            }
+        }
+
+        private double SetLuminosityByClass(){
+            switch (luminosityClass){
+                case LuminosityClass.O: return Random.Range(100000.0f,120000.0f);
+                case LuminosityClass.Iap: return Random.Range(70000f,100000f);
+                case LuminosityClass.Ia: return Random.Range(30000.0f,70000.0f);
+                case LuminosityClass.Ib: return Random.Range(7000f,30000f);
+                case LuminosityClass.II: return Random.Range(1000f,7000f);
+                case LuminosityClass.III: return Random.Range(90f,1000f);
+                case LuminosityClass.IV: 
+                    if((int)spectralClass<3) return Random.Range(100f,600f);
+                    return Random.Range(10,50);
+                case LuminosityClass.V: 
+                    if((int)spectralClass<3) return Random.Range(10f,500f);
+                    if((int)spectralClass<4) return Random.Range(1f,10f);
+                    if((int)spectralClass<6) return Random.Range(0.1f,1);
+                    if((int)spectralClass<7)return Random.Range(0.001f,0.1f);
+                    return Random.Range(0.00001f,0.001f);
+                case LuminosityClass.VI: 
+                    if((int)spectralClass<3) return Random.Range(1,100);
+                    if((int)spectralClass<4) return Random.Range(0.1f,1f);
+                    if((int)spectralClass<9) return Random.Range(0.01f,0.1f);
+                    return Random.Range(0.0001f,0.01f);
+                case LuminosityClass.VII: return Random.Range(0.0001f,0.01f);
+                default: throw new Exception($"Unknown star luminosity class {luminosityClass}");
+            }
+        }
+
+        public void SetLodColor(Color color){
+            lodSprite.material.SetColor("_BaseColor", color);
+            float normal = Mathf.InverseLerp(0.001f, 130000f, (float)luminosity);
+            float normalLum = Mathf.Lerp(1f, 2f, normal);
+            lodSprite.size = new Vector2(normalLum,normalLum);
+        }
+    }
+}
